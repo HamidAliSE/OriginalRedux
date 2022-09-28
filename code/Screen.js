885b6a1a8910a68c638ae3651dc0930cdfb92712
Hamid Ali <hamidalig942@gmail.com>
2022-09-28 09:52:56 +0500

    refactored code

--- code/Screen.js
+++ code/Screen.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
-import { CAKE_ORDERED } from './redux/cake/cakeTypes';
-import { orderCake } from './redux/cake/cakeActions';
 
-import store from './redux/store';
+import {useSelector, useDispatch} from 'react-redux';
+
+import {orderCake} from './redux/cake/cakeActions';
 
 const Screen = () => {
   const dispatch = useDispatch();
   const numOfCakes = useSelector(state => state.numOfCakes);
-  console.log('Initial State *** : ', store.getState())
+
   return (
     <View style={styles.screen}>
-        <Text>No. of cakes = {numOfCakes}</Text>
-        <Button
-          title="Order Cake"
-          onPress={() => dispatch(orderCake())}
-        />
+      <Text>No. of cakes = {numOfCakes}</Text>
+      <Button title="Order Cake" onPress={() => dispatch(orderCake())} />
     </View>
   );
 };
